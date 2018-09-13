213fe52bac8961d8aa585696e95a4aa5d45c5241
Vincent Cao <v.cao@sap.com>
2018-09-13 11:12:41 +0800

    if else语句

--- processorder.php
+++ processorder.php
@@ -9,10 +9,22 @@
 			$tireqty=$_POST['tireqty'];
 			$oilqty=$_POST['oilqty'];
 			$sparkqty=$_POST['sparkqty'];
+			$find=$_POST['find'];
 			echo'<p>Your order is as follows:</p>';
 			echo$tireqty.'tires<br/>';
 			echo$oilqty.'bottles of oil<br/>';
 			echo$sparkqty.'spark plugs<br/>';
+			if($find=="a") {
+				echo"<p>Regular customer.</p>";
+			}elseif($find=="b"){
+				echo"<p>Customer referred by TV advert.</p>";
+			}elseif($find=="c"){
+				echo"<p>Customer referred by phone directory</p>";
+			}elseif($find=="d"){
+				echo"<p>Customer referred by word of month</p>";
+			}else {
+				echo"<p>We do not know how the customer found us</p>";
+			}
 		?>
 	</h2>
 </html>
